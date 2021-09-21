34dfed55e15ab21c2fc6b1fb38e6cfa6f4ee26ec
Artur Derybowski <80964472+Vaxuss@users.noreply.github.com>
2021-09-21 13:30:42 +0200

    Zad6 i wektory

---
+++ DwuWymiaroweZad7C++/DwuWymiaroweZad7C++/DwuWymiaroweZad7C++.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+void sort()
+{
+
+}
+
+int main()
+{
+    srand(time(NULL));
+    int** tab2 = new int* [5];
+    vector <int> wektor = {};
+    int pole = 1;
+    int suma = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        tab2[i] = new int[5];
+        for (int j = 0; j < 5; j++)
+        {
+            tab2[i][j] = rand()%25 +1;
+            cout << tab2[i][j] << " ";
+        }
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {          
+            wektor.push_back(abs(tab2[i][j] - pole));
+            pole++;
+        }
+    }
+
+    for (int element : wektor)
+    {
+        cout << element << " ";       
+        suma += element;
+    }
+
+    cout << endl << suma;
+    
+    for (int i = 0; i < 5; i++)
+    {
+        delete[] tab2[i];
+    }
+
+    delete[] tab2;
+}
