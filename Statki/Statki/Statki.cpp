712f544df10a9cf2600ff763f677982a16dfc357
Artur Derybowski <80964472+Vaxuss@users.noreply.github.com>
2021-10-04 11:31:32 +0200

    Statki do naprawy

--- Statki/Statki/Statki.cpp
+++ Statki/Statki/Statki.cpp
@@ -13,6 +13,7 @@ void umiesc_statek(int a, int plansza[7][7], vector <int> statki[3]) {
         ok = true;
         cout << "podaj " << a << " - masztowiec: ";
         cin >> x >> y >> z;
+
         if (z == 0) { //poziomo
             if (x > 6 - a) {
                 cout << "podales zla lokalizacje dla wspolrzednej X";
@@ -152,6 +153,14 @@ void sortowanie_przez_wstawianie(int tab[][2], int n) {
     }
 }
 
+void tablica_traf(int a, int b, int komp[25][2], int tr[4][2],int ii, int m)
+{
+    if ((komp[m][0] == a) && (komp[m][1] == b)) {
+        tr[ii][0] = a;
+        tr[ii++][1] = b;
+    }
+}
+
 int main()
 {
     srand(time(NULL));
@@ -169,7 +178,7 @@ int main()
 
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++) {
-            plansza11[i][j] = '.'; //Y X
+            plansza11[i][j] = '*'; //Y X
 
         }
     for (int i = 1; i <= 5; i++)
@@ -185,15 +194,14 @@ int main()
     umiesc_statek(2, plansza1, statki1);
     umiesc_statek(1, plansza1, statki1);
     umiesc_statek(1, plansza1, statki1);
-
     umiesc_statek_k(3, plansza2, statki2);
     umiesc_statek_k(2, plansza2, statki2);
     umiesc_statek_k(1, plansza2, statki2);
     umiesc_statek_k(1, plansza2, statki2);
 
     bool a = true;
-    int traf[4][2] = { 0 };
     int temp[3][2] = { 0 };
+    int traf[4][2] = { 0 };
     while ((zatopiony < 4) && (zatopionyk < 4)) {
         int x, y, z, los, t, licz = 0;
 
@@ -225,8 +233,7 @@ int main()
         }
         else {
             sortowanie_przez_wstawianie(traf, 4);
-            if (traf[0][0] == 0)
-            {
+            if (traf[0][0] == 0) {
                 los = rand() % licznik;
                 x = komputer[los][0];
                 y = komputer[los][1];
@@ -246,10 +253,8 @@ int main()
                         komputer[m][1] = 0;
                     }
             }
-
             cout << endl << "K: " << x << " " << y;
             z = strzal(x, y, plansza1, statki1);
-
             if (z == 2) {
                 cout << " zatopiony" << endl;
                 zatopionyk++;
@@ -290,23 +295,49 @@ int main()
                 if (t == 0) {
                     int ii = 0;
                     for (int m = 0; m < licznik; m++) {
-                        if ((komputer[m][0] == x - 1) && (komputer[m][1] == y)) {
-                            traf[ii][0] = x - 1;
-                            traf[ii++][1] = y;
-                        }
-                        if ((komputer[m][0] == x) && (komputer[m][1] == y - 1)) {
-                            traf[ii][0] = x;
-                            traf[ii++][1] = y - 1;
-                        }
-                        if ((komputer[m][0] == x + 1) && (komputer[m][1] == y)) {
-                            traf[ii][0] = x + 1;
-                            traf[ii++][1] = y;
-                        }
-                        if ((komputer[m][0] == x) && (komputer[m][1] == y + 1)) {
-                            traf[ii][0] = x;
-                            traf[ii++][1] = y + 1;
-                        }
+                        tablica_traf(x - 1, y, komputer, traf, ii, m);
+                        tablica_traf(x, y - 1, komputer, traf, ii, m);
+                        tablica_traf(x + 1, y, komputer, traf, ii, m);
+                        tablica_traf(x, y + 1, komputer, traf, ii, m);                    
                     }
+                }
+                else {
+                    cout << "-------------------------------";
+                    for (int i = 0; i < 4; i++) {
+                        traf[i][0] = 0;
+                        traf[i][1] = 0;
+                    }
+                    int ii = 0;
+                    if (temp[0][0] != temp[1][0])
+                        if (temp[0][0] > temp[1][0])
+                            for (int m = 0; m < licznik; m++) {
+                                tablica_traf(temp[0][0] - 1, temp[0][1], komputer, traf, ii, m);
+                                tablica_traf(temp[1][0] + 1, temp[1][1], komputer, traf, ii, m);
+                            }
+                        else
+                            for (int m = 0; m < licznik; m++) {
+                                tablica_traf(temp[0][0] - 1, temp[0][1], komputer, traf, ii, m);
+                                tablica_traf(temp[1][0] + 1, temp[1][1], komputer, traf, ii, m);
+                            }
+
+                    else
+                        if (temp[0][1] > temp[1][1])
+                            for (int m = 0; m < licznik; m++) {
+                                tablica_traf(temp[0][0], temp[0][1] - 1, komputer, traf, ii, m);
+                                tablica_traf(temp[1][0], temp[1][1] + 1, komputer, traf, ii, m);                              
+                            }
+                        else
+                            for (int m = 0; m < licznik; m++) {
+                                if ((komputer[m][0] == temp[0][0]) && (komputer[m][1] == temp[0][1] - 1)) {
+                                    traf[ii][0] = temp[0][0];
+                                    traf[ii++][1] = temp[0][1] - 1;
+                                }
+                                if ((komputer[m][0] == temp[1][0]) && (komputer[m][1] == temp[1][1] + 1)) {
+                                    traf[ii][0] = temp[1][0];
+                                    traf[ii++][1] = temp[1][1] + 1;
+                                }
+                            }
+
                 }
             }
             else {
@@ -316,6 +347,7 @@ int main()
             sortowanie_przez_wstawianie(komputer, 25);
             if (licz != 0) licznik -= licz;
             else licznik--;
+            cout << "--licznik: " << licznik << endl;
         }
     }
     if (zatopiony == 4) cout << "wygral gracz";
