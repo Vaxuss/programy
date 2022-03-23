b17052e96baac9bba7f7ce3858fd444adcba08a0
Vaxuss <a.derybowski@wp.pl>
2022-03-23 22:53:21 +0100

    yhy

--- klasyCzyCos/klasyCzyCos/klasyCzyCos.cpp
+++ klasyCzyCos/klasyCzyCos/klasyCzyCos.cpp
@@ -4,135 +4,135 @@
 
 using namespace std;
 
-//class Prostokat {
-//private:
-//	double bok1;
-//	double bok2;
-//
-//public:
-//	Prostokat();
-//	Prostokat(double, double);
-//	Prostokat(const Prostokat&);//konstruktor kopiujący;
-//
-//	void pobierzBoki(double&, double&);
-//	void ustawBoki(double, double);
-//	double pole();
-//	double obwod();
+class Prostokat {
+private:
+	double bok1;
+	double bok2;
+
+public:
+	Prostokat();
+	Prostokat(double, double);
+	Prostokat(const Prostokat&);//konstruktor kopiujący;
+
+	void pobierzBoki(double&, double&);
+	void ustawBoki(double, double);
+	double pole();
+	double obwod();
+};
+
+Prostokat::Prostokat() {
+
+}
+
+Prostokat::Prostokat(double pBok1, double pBok2) {
+	bok1 = pBok1;
+	bok2 = pBok2;
+}
+
+Prostokat::Prostokat(const Prostokat& wzorzec) {
+	bok1 = wzorzec.bok1;
+	bok2 = wzorzec.bok2;
+}
+
+void Prostokat::pobierzBoki(double& pBok1, double& pBok2) {
+	pBok1 = bok1;
+	pBok2 = bok2;
+}
+
+void Prostokat::ustawBoki(double pBok1, double pBok2) {
+	bok1 = pBok1;
+	bok2 = pBok2;
+}
+
+double Prostokat::pole() {
+	return bok1 * bok2;
+}
+
+double Prostokat::obwod() {
+	return 2 * bok1 + 2 * bok2;
+}
+
+Prostokat kopiujProstokat(Prostokat prostokat) {
+	return prostokat;
+}
+
+int main()
+{
+	double b1, b2;
+
+	Prostokat p1(1, 2);
+	p1.pobierzBoki(b1, b2);
+	cout << b1 << " " << b2 << endl;
+
+	Prostokat p2 = p1;
+	p2.pobierzBoki(b1, b2);
+	cout << b1 << " " << b2 << endl;
+
+	//kopiowanie na bazie wzorca
+	Prostokat p3(p1);
+	p3.pobierzBoki(b1, b2);
+	cout << b1 << " " << b2 << endl;
+
+	Prostokat p4;
+	p4 = kopiujProstokat(p1);
+	cout << b1 << " " << b2 << endl;
+	
+}
+
+//struct Data {
+//	int dd, mm, rr;
 //};
 //
-//Prostokat::Prostokat() {
-//
-//}
-//
-//Prostokat::Prostokat(double pBok1, double pBok2) {
-//	bok1 = pBok1;
-//	bok2 = pBok2;
-//}
-//
-//Prostokat::Prostokat(const Prostokat& wzorzec) {
-//	bok1 = wzorzec.bok1;
-//	bok2 = wzorzec.bok2;
-//}
-//
-//void Prostokat::pobierzBoki(double& pBok1, double& pBok2) {
-//	pBok1 = bok1;
-//	pBok2 = bok2;
-//}
-//
-//void Prostokat::ustawBoki(double pBok1, double pBok2) {
-//	bok1 = pBok1;
-//	bok2 = pBok2;
-//}
-//
-//double Prostokat::pole() {
-//	return bok1 * bok2;
-//}
-//
-//double Prostokat::obwod() {
-//	return 2 * bok1 + 2 * bok2;
-//}
-//
-//Prostokat kopiujProstokat(Prostokat prostokat) {
-//	return prostokat;
-//}
-//
-//int main()
-//{
-//	double b1, b2;
-//
-//	Prostokat p1(1, 2);
-//	p1.pobierzBoki(b1, b2);
-//	cout << b1 << " " << b2 << endl;
-//
-//	Prostokat p2 = p1;
-//	p2.pobierzBoki(b1, b2);
-//	cout << b1 << " " << b2 << endl;
-//
-//	//kopiowanie na bazie wzorca
-//	Prostokat p3(p1);
-//	p3.pobierzBoki(b1, b2);
-//	cout << b1 << " " << b2 << endl;
-//
-//	Prostokat p4;
-//	p4 = kopiujProstokat(p1);
-//	cout << b1 << " " << b2 << endl;
+//class Pracownik {
+//public:
+//	int id;
+//	string imie, nazwisko;
+//	Data data_urodzenia;
+//	//prototyp
+//	Pracownik();
+//	Pracownik(int, string, string);
+//	Pracownik(int, string, string, Data);
 //	
+//	void wyswietlDane() {
+//		cout << id << ": " << imie << " " << nazwisko << endl
+//			<< data_urodzenia.dd << "."
+//			<< data_urodzenia.mm << "."
+//			<< data_urodzenia.rr<<endl;
+//	}
+//};
+//
+//Pracownik::Pracownik() {
+//	id = -1;
+//	imie = "AAAAAA";
+//	nazwisko = "AAA";
+//	data_urodzenia = { 1 , 1, 1900 };
 //}
-
-struct Data {
-	int dd, mm, rr;
-};
-
-class Pracownik {
-public:
-	int id;
-	string imie, nazwisko;
-	Data data_urodzenia;
-	//prototyp
-	Pracownik();
-	Pracownik(int, string, string);
-	Pracownik(int, string, string, Data);
-	
-	void wyswietlDane() {
-		cout << id << ": " << imie << " " << nazwisko << endl
-			<< data_urodzenia.dd << "."
-			<< data_urodzenia.mm << "."
-			<< data_urodzenia.rr<<endl;
-	}
-};
-
-Pracownik::Pracownik() {
-	id = -1;
-	imie = "AAAAAA";
-	nazwisko = "AAA";
-	data_urodzenia = { 1 , 1, 1900 };
-}
-
-Pracownik::Pracownik(int pId, string pImie, string pNazwisko) {
-	id = pId;
-	imie = pImie;
-	nazwisko = pNazwisko;
-}
-
-//konstruktor poza klasą;
-Pracownik::Pracownik(int pId, string pImie, string pNazwisko, Data pDaraUrodzenia) {
-	id = pId;
-	imie = pImie;
-	nazwisko = pNazwisko;
-	data_urodzenia = pDaraUrodzenia;
-}
-
-int main() {
-	Pracownik pracownik1;
-	pracownik1.wyswietlDane();
-
-	Pracownik pracownik2;
-	pracownik2 = { 1, "Jan", "Kowalski" };
-	pracownik2.wyswietlDane();
-	
-	Pracownik pracownik3(10, "adam", "Nowak", {10,10,2000});
-	pracownik3.wyswietlDane();
-}
+//
+//Pracownik::Pracownik(int pId, string pImie, string pNazwisko) {
+//	id = pId;
+//	imie = pImie;
+//	nazwisko = pNazwisko;
+//}
+//
+////konstruktor poza klasą;
+//Pracownik::Pracownik(int pId, string pImie, string pNazwisko, Data pDaraUrodzenia) {
+//	id = pId;
+//	imie = pImie;
+//	nazwisko = pNazwisko;
+//	data_urodzenia = pDaraUrodzenia;
+//}
+//
+//int main() {
+//	Pracownik pracownik1;
+//	pracownik1.wyswietlDane();
+//
+//	Pracownik pracownik2;
+//	pracownik2 = { 1, "Jan", "Kowalski" };
+//	pracownik2.wyswietlDane();
+//	
+//	Pracownik pracownik3(10, "adam", "Nowak", {10,10,2000});
+//	pracownik3.wyswietlDane();
+//}
 
 //------------------------------------------------------------------------------------------
 
