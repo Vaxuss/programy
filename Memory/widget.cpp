cff048ba031bc6b3bbd7394c9c713165e55a0b05
Artur Derybowski <80964472+Vaxuss@users.noreply.github.com>
2022-04-12 15:50:29 +0200

    memory

---
+++ Memory/widget.cpp
@@ -0,0 +1,21 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QTimer>
+
+using namespace std;
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+
+
