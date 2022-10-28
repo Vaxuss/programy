8606ac29e6b609881a56a0e316f53a01ddc3a519
Artur Derybowski <80964472+Vaxuss@users.noreply.github.com>
2022-10-28 13:03:47 +0200

    Trigers WPF

---
+++ Triggery/BoolVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Triggery
+{
+    [ValueConversion(typeof(bool),typeof(Visibility))]
+    public class BoolVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isVisible = (bool)value;
+            if (isVisible)
+            {
+                return Visibility.Visible;
+            }
+            else return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
