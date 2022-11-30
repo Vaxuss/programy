33ba6715f10c91edda77dfea9df6fe09d7d10ac9
Vaxuss <a.derybowski@wp.pl>
2022-11-30 23:41:11 +0100

    nie wiem 30.11.22

--- Triggery/obj/Debug/net6.0-windows/App.g.cs
+++ Triggery/obj/Debug/net6.0-windows/App.g.cs
@@ -47,7 +47,7 @@ namespace Triggery {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.11.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace Triggery {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.11.0")]
         public static void Main() {
             Triggery.App app = new Triggery.App();
             app.InitializeComponent();
--- Triggery/obj/Debug/net6.0-windows/App.g.i.cs
+++ Triggery/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace Triggery {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.11.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace Triggery {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Triggery;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Triggery;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace Triggery {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.11.0")]
         public static void Main() {
             Triggery.App app = new Triggery.App();
             app.InitializeComponent();
